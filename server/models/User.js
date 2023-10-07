c895545fbe8f96d913690beeb97dc714286c8a57
barre <barrenac@oregonstate.edu>
2023-10-07 15:11:36 -0700

    created user model and added boiler plate code to repo.

---
+++ server/models/User.js
@@ -0,0 +1,73 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,  
+    required: true, 
+    trim: true
+  },
+  email: {
+    type: String, 
+    required: true, 
+    unique: true, 
+    match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email.'] 
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  posts: [
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'post',
+    }
+  ],
+  votes: [
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+    }
+]
+},
+{
+  toJSON: {
+      virtuals: true,
+  },
+  id: false,
+}
+);
+
+userSchema
+    .virtual('postCount')
+    .get(function () {
+        return this.posts.length
+    });
+
+userSchema
+    .virtual('voteCount')
+    .get(function () {
+        return this.votes.length
+    });
+
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const User = model('user', userSchema);
+
+module.exports = User;
