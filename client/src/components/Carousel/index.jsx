f8c6548ca6c909971e1f125a8aca169543652485
barre <barrenac@oregonstate.edu>
2023-10-18 16:25:22 -0700

    fixed carousel display issues with place holder images.

--- client/src/components/Carousel/index.jsx
+++ client/src/components/Carousel/index.jsx
@@ -4,9 +4,6 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import '../../styles/home.css'
 
-import CarouselPhotoOne from "../../assets/images/carousel-photo-one.png";
-import CarouselPhotoTwo from "../../assets/images/carousel-photo-two.png";
-import CarouselPhotoThree from "../../assets/images/carousel-photo-three.png";
 
 function Carousel() {
   const settings = {
@@ -24,13 +21,13 @@ function Carousel() {
         <Container >
         <Slider {... settings}>
           <div className="carousel-image-box">
-            <img className="carousel-image" src={CarouselPhotoOne} alt="app photo one"/>
+            <img className="carousel-image" src="https://i.ytimg.com/vi/2L_D5fBq1ao/maxresdefault.jpg" alt="goku vs luffy"/>
           </div>
           <div className="carousel-image-box">
-            <img className="carousel-image" src={CarouselPhotoTwo} alt="app photo one"/>
+            <img className="carousel-image" src="https://i.ytimg.com/vi/NtMkWjmBVe8/maxresdefault.jpg" alt="kakshi vs gojo"/>
           </div>
           <div className="carousel-image-box">
-            <img className="carousel-image" src={CarouselPhotoThree} alt="app photo one"/>
+            <img className="carousel-image" src="https://i.ytimg.com/vi/ZvLxn3km2qs/maxresdefault.jpg" alt="Marvel vs DC"/>
           </div>
           </Slider>
         </Container>
