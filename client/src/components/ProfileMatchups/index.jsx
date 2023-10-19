df67bf3e16969e8ed81b561b53870c343c959cf6
barre <barrenac@oregonstate.edu>
2023-10-19 14:08:30 -0700

    removed vote button from profile

--- client/src/components/ProfileMatchups/index.jsx
+++ client/src/components/ProfileMatchups/index.jsx
@@ -20,32 +20,6 @@ const ProfileDashboard = () => {
   const handleVote = (optionNum) => {
     console.log(optionNum);
   }
-  // //deleting the user's posts
-  // const [formState, setFormState] = useState({
-  //   _id: "",
-  // });
-  // const [removePost] = useMutation(REMOVE_POST);
-
-  // const handleFormSubmit = async (event) => {
-  //   event.preventDefault();
-  //   try {
-  //     const { data: mutationData } = await removePost({
-  //       variables: {
-  //         _id: formState.id,
-  //       },
-  //     });
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-  // const handleChange = (event) => {
-  //   const { name, value } = event.target;
-
-  //   setFormState({
-  //     ...formState,
-  //     [name]: value,
-  //   });
-  // };
 
   const posts = data?.userPosts || [];
   return (
@@ -65,33 +39,16 @@ const ProfileDashboard = () => {
                         <div className="content">
                           <div className="header">{post.option1}</div>
                           <div className="meta">{post.option1_votes} Votes</div>
-                          <button className="btn btn-info" onClick={() => handleVote(1)}>
-                            Vote
-                          </button>
                         </div>
                       </div>
                       <div className="ui green segment">
                         <div className="content">
                           <div className="header">{post.option2}</div>
                           <div className="meta">{post.option2_votes} Votes</div>
-                          <button className="btn btn-info" onClick={() => handleVote(2)}>
-                            Vote
-                          </button>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div className="extra content">
-                  {/* <button className="ui button">See Comments</button> */}
-                  {/* <Button
-                    className=" ui button right floated star"
-                    onChange={handleChange}
-                    value={formState.id}
-                    input={post.id}
-                  >
-                    Delete Post
-                  </Button> */}
                 </div>
               </div>
             ))}
